baff6642160775a50c4012d925b7c0eb369bcf1b
Michael Fahim <michael_fahim@yahoo.com>
2020-02-26 02:39:03 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,24 @@
 class Triangle
   # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a, @b, @c = a, b, c
+  end
+
+  def kind
+    min, min2, max = [@a, @b, @c].sort
+    if @a < 0 || @b < 0 || @c < 0 || min + min2 <= max
+      raise TriangleError
+    end
+
+    if @a == @b && @b == c
+      :equilateral
+    elsif @a == @b || @a == @c || @b == @c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
 end
